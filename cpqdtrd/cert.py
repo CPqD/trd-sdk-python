a5a37bb39304a0a50a9aa1e113424461e3a7db09
Fabiano Zaruch Chinasso <fzaruch@cpqd.com.br>
2026-05-05 14:33:25 -0300

    [TRD-750] Fix due new cryptography library in Ubuntu 24.04

--- cpqdtrd/cert.py
+++ cpqdtrd/cert.py
@@ -1,40 +1,56 @@
 # -*- coding: utf-8 -*-
-from OpenSSL import crypto
+from cryptography import x509
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.x509.oid import NameOID
+import ipaddress
+from datetime import datetime, timedelta, timezone
 import re
 
 
 def create_self_signed_cert(hostname, cert_path, pkey_path):
     """Generate a certificate and private key, and returns the public key as str."""
-    # create a key pair
-    k = crypto.PKey()
-    k.generate_key(crypto.TYPE_RSA, 2048)
-
-    # create a self-signed cert
-    cert = crypto.X509()
-    cert.get_subject().C = "BR"
-    cert.get_subject().ST = "Sao Paulo"
-    cert.get_subject().L = "Campinas"
-    cert.get_subject().O = hostname
-    cert.get_subject().OU = hostname
-    cert.set_serial_number(1000)
-    cert.gmtime_adj_notBefore(0)
-    cert.gmtime_adj_notAfter(10 * 365 * 24 * 60 * 60)
-    cert.set_issuer(cert.get_subject())
-
-    if re.compile(r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}").match(hostname):
-        alt_name = "IP:" + hostname
-    else:
-        alt_name = "DNS:" + hostname
-    cert.add_extensions(
-        [crypto.X509Extension(b"subjectAltName", False, alt_name.encode())]
+    key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+    now = datetime.now(timezone.utc)
+    subject = issuer = x509.Name(
+        [
+            x509.NameAttribute(NameOID.COUNTRY_NAME, "BR"),
+            x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, "Sao Paulo"),
+            x509.NameAttribute(NameOID.LOCALITY_NAME, "Campinas"),
+            x509.NameAttribute(NameOID.ORGANIZATION_NAME, hostname),
+            x509.NameAttribute(NameOID.ORGANIZATIONAL_UNIT_NAME, hostname),
+        ]
     )
 
-    cert.set_pubkey(k)
-    cert.sign(k, "sha512")
+    if re.compile(r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}").match(hostname):
+        subject_alt_name = x509.IPAddress(ipaddress.ip_address(hostname))
+    else:
+        subject_alt_name = x509.DNSName(hostname)
+
+    cert = (
+        x509.CertificateBuilder()
+        .subject_name(subject)
+        .issuer_name(issuer)
+        .public_key(key.public_key())
+        .serial_number(1000)
+        .not_valid_before(now)
+        .not_valid_after(now + timedelta(days=3650))
+        .add_extension(
+            x509.SubjectAlternativeName([subject_alt_name]),
+            critical=False,
+        )
+        .sign(private_key=key, algorithm=hashes.SHA512())
+    )
 
     with open(cert_path, "wb") as f:
-        f.write(crypto.dump_certificate(crypto.FILETYPE_PEM, cert))
+        f.write(cert.public_bytes(serialization.Encoding.PEM))
     with open(pkey_path, "wb") as f:
-        f.write(crypto.dump_privatekey(crypto.FILETYPE_PEM, k))
+        f.write(
+            key.private_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PrivateFormat.TraditionalOpenSSL,
+                encryption_algorithm=serialization.NoEncryption(),
+            )
+        )
 
     return
